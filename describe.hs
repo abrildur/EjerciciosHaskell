4f1bc5383403d415d47fc2aaafaa51d327741f54
Abril Daniela <71578178+abrildur@users.noreply.github.com>
2021-07-20 06:07:10 +0000

    conejos se cambio el archivo de describe.hs

--- describe.hs
+++ describe.hs
@@ -2,4 +2,4 @@ describeList :: [a] -> String
 describeList xs = "The list is " ++ what xs
     where what [] = "empty."
           what [x] = "a singleton list."
-          what xs = "a longer list."
+          what xs = "a longer lista."
